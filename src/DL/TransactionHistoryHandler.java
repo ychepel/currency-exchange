23b1015efc763411e28bc84e90b04da8074d7f0d
Algalon <kirill.bernatskyi@gmail.com>
2024-01-29 01:03:44 +0100

    add method read from file

--- src/DL/TransactionHistoryHandler.java
+++ src/DL/TransactionHistoryHandler.java
@@ -1,7 +1,10 @@
 package DL;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TransactionHistoryHandler {
     private final String path;
@@ -21,5 +24,26 @@ public class TransactionHistoryHandler {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+
+    public ArrayList<Transaction> read(){
+        ArrayList<Transaction> result = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File(this.path))){
+            while (scanner.hasNextLine()) {
+                String newTask = scanner.nextLine();
+                String[] propertiesOfTransaction = newTask.split(";");
+                Transaction transaction = new Transaction(
+                        propertiesOfTransaction[0],
+                        Double.parseDouble(propertiesOfTransaction[1].replace(",",".")),
+                        CurrencyTitle.valueOf(propertiesOfTransaction[2]),
+                        CurrencyTitle.valueOf(propertiesOfTransaction[3]),
+                        Double.parseDouble(propertiesOfTransaction[4].replace(",",".")));
+                result.add(transaction);
+            }
+            return result;
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 }
