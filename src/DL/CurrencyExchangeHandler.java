e204fc3ab3430df625b59715da543984d38b340c
Algalon <kirill.bernatskyi@gmail.com>
2024-01-29 01:31:49 +0100

    Fixes regarding PR

--- src/DL/CurrencyExchangeHandler.java
+++ src/DL/CurrencyExchangeHandler.java
@@ -8,16 +8,16 @@ import java.util.Scanner;
 public class CurrencyExchangeHandler {
     private File file;
 
-    public CurrencyExchangeHandler(){
-        this.file = new File("src/rates.txt");
+    public CurrencyExchangeHandler(String path){
+        this.file = new File(path);
     }
 
     public HashMap<CurrencyTitle, Double> read(){
         HashMap<CurrencyTitle, Double> hashMapList = new HashMap<>();
         try (Scanner scanner = new Scanner(this.file)){
             while (scanner.hasNextLine()){
-                String CurrencyRate = scanner.nextLine();
-                String[] preHashMap = CurrencyRate.split(",");
+                String currencyRate = scanner.nextLine();
+                String[] preHashMap = currencyRate.split(",");
                 hashMapList.put(CurrencyTitle.valueOf(preHashMap[0]),Double.parseDouble(preHashMap[1]));
             }
         }catch (FileNotFoundException e){
