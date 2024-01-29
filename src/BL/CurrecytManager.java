7fd30c68bf955947359458cbb33592c688ead388
katerinaonix8585 <katerinaonix8585@gmail.com>
2024-01-29 16:50:53 +0100

    change rename DEFAULT_CURRENCY, new property, change property rateFilePath,

--- src/BL/CurrecytManager.java
+++ src/BL/CurrecytManager.java
@@ -4,13 +4,14 @@ import java.util.HashMap;
 
 public class CurrencyManager {
     //Определяет статическую константу - значение валюты по умолчанию
-    public static final CurrencyTitle defaultCurrency = CurrencyTitle.EUR;
+    public static final CurrencyTitle DEFAULT_CURRENCY = CurrencyTitle.EUR;
     // Определяет приватное поле data, предназначенное для хранения экземпляра класса CurrencyExchangeHandler
+    private String rateFilePath = "src/rate.txt";
     private final CurrencyExchangeHandler data;
 
     // Конструктор
-    public CurrentManager(String rateFilePath) {
-        this.data = new CurrencyExchangeHandler(rateFilePath);
+    public CurrencyManager() {
+        this.data = new CurrencyExchangeHandler(this.rateFilePath);
     }
 
     /**
@@ -18,7 +19,7 @@ public class CurrencyManager {
      * Использует метод read() класса CurrencyExchangeHandler.
      * Метод возвращает HashMap с валютами и их курсами.
     **/
-     public HashMap<String, Double> getAllCurrency () {
+     public HashMap<CurrencyTitle, Double> getAllCurrency () {
         return data.read();
     }
 
@@ -31,9 +32,9 @@ public class CurrencyManager {
 
         //TODO - предусмотреть случай, когда курс = 0
 
-        if (initialCurrency.equals(defaultCurrency)) {
+        if (initialCurrency.equals(DEFAULT_CURRENCY)) {
             return 1 / exchangeRate.get(resultCurrency);
-        } else if (resultCurrency.equals(defaultCurrency)) {
+        } else if (resultCurrency.equals(DEFAULT_CURRENCY)) {
             return exchangeRate.get(initialCurrency);
         } else {
             return exchangeRate.get(initialCurrency) / exchangeRate.get(resultCurrency);
@@ -47,5 +48,6 @@ public class CurrencyManager {
     public double calculateTotalAmount (double InitialCurrencyAmount,CurrencyTitle initialCurrency, CurrencyTitle resultCurrency) {
         return calculateRate(initialCurrency, resultCurrency) * InitialCurrencyAmount;
 
-}
+
+    }
 }
